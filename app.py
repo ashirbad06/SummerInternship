babb485ff5156f73b5c80e875f021ed315b3e567
ashirbad <aashirbadpanda001@gmail.com>
2023-07-20 07:31:00 +0530

    Main commit

--- app.py
+++ app.py
@@ -1,34 +1,55 @@
+import pandas as pd
 from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 
 app = Flask(__name__, template_folder='templates')
 
+dataset = pd.read_csv('C:\\Users\\admin\\Desktop\\PS1 docs\\Symptom-se.csv')
+
+conversations = []
+
+try:
+    for _, row in dataset.iterrows():
+        conversation = [row['user'], row['bot']]
+        conversations.append(conversation)
+except KeyError as e:
+    print(f"Error accessing column: {e}")
+
+# Create a ChatBot instance
+chatbot = ChatBot('MyChatBot')
+
+# Create a ListTrainer instance for custom dataset training
+list_trainer = ListTrainer(chatbot)
+list_trainer.train(conversations)
+
+# Create a ChatterBotCorpusTrainer instance for built-in corpus training
+corpus_trainer = ChatterBotCorpusTrainer(chatbot)
+corpus_trainer.train('chatterbot.corpus.english')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.form['user_input']
+    response = chatbot.get_response(user_input)
+    return str(response)
 
 @app.route('/', methods=['GET', 'POST'])
 def symptom_assessment():
     if request.method == 'POST':
-        intensity = request.form['intensity']
-        location = request.form['location']
-        duration = request.form['duration']
-        aggravating_factors = request.form['aggravating_factors']
-        alleviating_factors = request.form['alleviating_factors']
-        other_symptoms = request.form['other_symptoms']
-        previous_treatment = request.form['previous_treatment']
-        additional_info = request.form['additional_info']
         # Process the collected information (e.g., store in a database, perform calculations)
-        
+        # ...
+
         return render_template('summary.html',
-                               intensity=intensity,
-                               location=location,
-                               duration=duration,
-                               aggravating_factors=aggravating_factors,
-                               alleviating_factors=alleviating_factors,
-                               other_symptoms=other_symptoms,
-                               previous_treatment=previous_treatment,
-                               additional_info=additional_info)
-    
+                               intensity=request.form['intensity'],
+                               location=request.form['location'],
+                               duration=request.form['duration'],
+                               aggravating_factors=request.form['aggravating_factors'],
+                               alleviating_factors=request.form['alleviating_factors'],
+                               other_symptoms=request.form['other_symptoms'],
+                               previous_treatment=request.form['previous_treatment'],
+                               additional_info=request.form['additional_info'])
+
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)  # Change the port number to a different value
-
+    app.run(debug=True, port=8000)
